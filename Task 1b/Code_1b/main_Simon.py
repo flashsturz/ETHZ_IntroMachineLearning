f4e1198de7c840481094c4332ba1f8c36a6bd0fb
sitobler <sitobler@student.ethz.ch>
2021-03-24 16:05:45 +0100

    Draft.

--- Task 1b/Code_1b/main_Simon.py
+++ Task 1b/Code_1b/main_Simon.py
@@ -9,8 +9,9 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet, ElasticNetCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error, make_scorer
 from sklearn.model_selection import RepeatedKFold
 from math import sqrt
 
@@ -30,10 +31,32 @@ def prepare_data(linear_X):
     Phi=np.concatenate((linear_X,squared_X,exp_X,cos_X,const_X),axis=1)
     return Phi
 
+def own_scoring(y, y_pred, **kwargs):
+    return sqrt(mean_squared_error(y, y_pred))
 #----------------------------------------------------------------------------------------------------------------------
 # Read in Data
 train_data=data('Data_1b/train.csv',1,2)
 
+ownscore=make_scorer(own_scoring)
+
 Phi=prepare_data(train_data.X)
 
+rkf = RepeatedKFold(n_splits=10, n_repeats=10,random_state=999)
+regr=ElasticNet(random_state=1234)
+parameters={'l1_ratio':[0.1,0.3,0.5,0.7,0.9,0.95,0.98,0.99],'alpha':[0.001,0.01,0.1,0.25,0.5,1,10,100]}
 
+gscv = GridSearchCV(regr, param_grid=parameters,cv=rkf,scoring=ownscore,verbose=-1)
+gscv.fit(Phi,train_data.y)
+
+pd.DataFrame(gscv.cv_results_).to_csv("output.csv")
+best_estim=gscv.best_estimator_
+print("Best Estimator Parameters found by GridSearch:")
+print(gscv.best_params_)
+
+print(best_estim.coef_)
+pd.DataFrame(gscv.best_estimator_.coef_).to_csv("Estimtor_coef.csv")
+
+regrbest=ElasticNetCV(l1_ratio=[0.1,0.3,0.5,0.7,0.9,0.95,0.98,0.99],random_state=4321)
+regrbest.fit(Phi,train_data.y)
+
+print(regrbest.coef_)
