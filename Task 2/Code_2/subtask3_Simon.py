7c1cb08205cd3d19c670b002e8936f3b2c0356ef
sitobler <sitobler@student.ethz.ch>
2021-04-19 15:09:53 +0200

    Some changes to get more insight.

--- Task 2/Code_2/subtask3_Simon.py
+++ Task 2/Code_2/subtask3_Simon.py
@@ -36,9 +36,9 @@ def prepare_Xmat(features_pd):
         append_X=append_X[2:].flatten()
         X=np.vstack((X,append_X))
 
-        i=i+1
-        if(i%1000==0):
-            print("Finished %d pid's for X matrix..." % i)
+        #i=i+1
+        #if(i%1000==0):
+        #    print("Finished %d pid's for X matrix..." % i)
     X_np = np.array(X)
 
     print("Finished X-matrix.")
@@ -54,9 +54,9 @@ def prepare_Ymat(labels_pd):
         append_y=labels_pd.loc[labels_pd['pid']==pid].to_numpy()
         y=np.vstack((y, append_y))
 
-        i=i+1
-        if(i%1000==0):
-            print("Finished %d pid's for y matrix..." % i)
+        #i=i+1
+        #if(i%1000==0):
+        #    print("Finished %d pid's for y matrix..." % i)
     y_np = y[:,12:16]
 
     print("Finished y-matrix.")
@@ -82,7 +82,6 @@ def compute_Score(TRAIN_FEATURES,TRAIN_LABELS,TEST_FEATURES,KFOLD_SPLITS,KFOLD_R
     X_test = prepare_Xmat(test_features)
     del test_features
 
-
     if (verbose >= 1):
         print("Finished to prepare X and Y matrices,Shape of X_train and Y_train is: ")
         print(np.shape(X_train))
@@ -90,13 +89,22 @@ def compute_Score(TRAIN_FEATURES,TRAIN_LABELS,TEST_FEATURES,KFOLD_SPLITS,KFOLD_R
         print("Regression starts...")
         print_elapsed_time(starttime)
 
-    ENreg = ElasticNet(random_state=1234,max_iter=10e5,tol=5e-4)
+    ENreg = ElasticNet(random_state=1234,max_iter=10e4,tol=5e-4)
     rkf = RepeatedKFold(n_splits=KFOLD_SPLITS, n_repeats=KFOLD_REPEATS, random_state=1234)
     paramgrid = {'l1_ratio': L1_RATIO, 'alpha': ALPHAS}
+    if (verbose >= 1):
+        print("The following parameter Grid is used for regression: ")
+        print(paramgrid)
 
     gscv = GridSearchCV(ENreg, param_grid=paramgrid, scoring='neg_root_mean_squared_error', n_jobs=2, cv=rkf)
+    if (verbose >= 1):
+        print("  Finished regression-prep, fit and predict starts:")
     gscv.fit(X_train, y=Y_train)
+    if (verbose >= 1):
+        print("   Fit finished...")
     Y_predict = gscv.predict(X_test)
+    if (verbose >= 1):
+        print("   predict finished. Preparing result files and outputs...")
 
     gcsv_results_pd = gscv.cv_results_
 
@@ -122,27 +130,27 @@ FILE_FEATURES='train_features_simpleImpute_mean.csv'
 FILE_LABELS='Data_2/train_labels.csv'
 TEST_FEATURES='test_features_simpleImpute_constant.csv'
 
-#importfiles=[{'name_of_compute': 'mean',
-#              'FILE_FEATURE':'train_features_simpleImpute_mean.csv',
-#              'FILE_LABELS': 'Data_2/train_labels.csv',
-#             'TEST_FEATURES':'test_features_simpleImpute_mean.csv'},
-#             {'name_of_compute': 'median',
-#             'FILE_FEATURE':'train_features_simpleImpute_median.csv',
-#             'FILE_LABELS': 'Data_2/train_labels.csv',
-#             'TEST_FEATURES': 'test_features_simpleImpute_median.csv'},
-#             {'name_of_compute': 'constant',
-#             'FILE_FEATURE':'train_features_simpleImpute_constant.csv',
-#             'FILE_LABELS': 'Data_2/train_labels.csv',
-#             'TEST_FEATURES': 'test_features_simpleImpute_constant.csv'}]
-
 importfiles=[{'name_of_compute': 'mean',
               'FILE_FEATURE':'train_features_simpleImpute_mean.csv',
               'FILE_LABELS': 'Data_2/train_labels.csv',
-             'TEST_FEATURES':'test_features_simpleImpute_mean.csv'}]
+             'TEST_FEATURES':'test_features_simpleImpute_mean.csv'},
+             {'name_of_compute': 'median',
+             'FILE_FEATURE':'train_features_simpleImpute_median.csv',
+             'FILE_LABELS': 'Data_2/train_labels.csv',
+             'TEST_FEATURES': 'test_features_simpleImpute_median.csv'},
+             {'name_of_compute': 'constant',
+             'FILE_FEATURE':'train_features_simpleImpute_constant.csv',
+             'FILE_LABELS': 'Data_2/train_labels.csv',
+             'TEST_FEATURES': 'test_features_simpleImpute_constant.csv'}]
+
+#importfiles=[{'name_of_compute': 'mean',
+#              'FILE_FEATURE':'train_features_simpleImpute_mean.csv',
+#              'FILE_LABELS': 'Data_2/train_labels.csv',
+#             'TEST_FEATURES':'test_features_simpleImpute_mean.csv'}]
 
 
-ALPHAS=[0.01,0.02,0.05,0.1,1,10,20,25,50,75,100]
-L1_RATIO=[0.1,0.25,0.5,0.75,0.9,0.95,0.99]
+ALPHAS=[0.01,0.05,0.1,1,10,20,25,50,75,100]
+L1_RATIO=[0.1,0.5,0.9,0.95,0.99]
 KERNELS=['precomputed']
 
 KFOLD_SPLITS=2
@@ -167,7 +175,7 @@ for strat in importfiles:
     FILE_LABELS=strat['FILE_LABELS']
     TEST_FEATURES=strat['TEST_FEATURES']
 
-    [labels_predict_pd,this_gscv_results]=compute_Score(FILE_FEATURES,FILE_LABELS,TEST_FEATURES,KFOLD_SPLITS,KFOLD_REPEATS,time_start_overall,0)
+    [labels_predict_pd,this_gscv_results]=compute_Score(FILE_FEATURES,FILE_LABELS,TEST_FEATURES,KFOLD_SPLITS,KFOLD_REPEATS,time_start_overall,1)
 
     labels_predict_pd.to_csv("predictedLabels_" + current_time_str + "_"+strat['name_of_compute']+ ".csv", index=False)
     pd.DataFrame(this_gscv_results).to_csv("gscv_results" + current_time_str + "_"+strat['name_of_compute']+ ".csv")
