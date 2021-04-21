cecd450c382398d7fce8ffbfd90406ba029191bf
sitobler <sitobler@student.ethz.ch>
2021-04-21 14:28:39 +0200

    Changed FeatureTransform file to import file.

--- Task 2/Code_2/FeatureTransform_simpleImp.py
+++ Task 2/Code_2/FeatureTransform_simpleImp.py
@@ -1,110 +1,124 @@
-# Janik Baumer, Flavio Regenass, Simon Tobler
-# jbaumer, flavior, sitobler
-#-----------------
-# Description Task 2
+# Simon Tobler sitobler
 
-#--------------------------------------------------------------------------------------------------
+# This Function implements methods to use for imputing the given patientdata in IML Task2 2021
+# Use this file as importfile
+# --------------------------------------------------------------------------------------------------
 
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
-import random
 import time as time
 
-def md_list2pdSeries(inputlist,pd_col_list,name):
-    [n_times,n_meas,n_pid]=np.shape(inputlist)
-    #print(n_times) #for debugging
-    #print(n_meas) #for debugging
-    #print(n_pid) #for debugging
-    stackedList = inputlist[:, :, 0]
-    for pid in range(1,n_pid):
-        stackedList=np.concatenate((stackedList,inputlist[:,:,pid]),axis=0)
-    #print(np.shape(stackedList)) #for debugging
 
-    pd_dataframe=pd.DataFrame(data=stackedList,columns=pd_col_list)
+def md_list2pdSeries(inputlist, pd_col_list):
+    [n_times, n_meas, n_pid] = np.shape(inputlist)
+    stacked_list = inputlist[:, :, 0]
+    for pid in range(1, n_pid):
+        stacked_list = np.concatenate((stacked_list, inputlist[:, :, pid]), axis=0)
 
-    filename="test_features_simpleImpute_"+name+".csv"
-    pd_dataframe.to_csv(filename,index=False)
+    pd_dataframe = pd.DataFrame(data=stacked_list, columns=pd_col_list)
+
+    return pd_dataframe
+
 
 def print_elapsed_time(starttime):
-    time_now=time.perf_counter()
-    elapsed_time=time_now-starttime
+    time_now = time.perf_counter()
+    elapsed_time = time_now-starttime
     print("Time elapsed since start: %.2f s" % elapsed_time)
 
-#--------------------------------------------------------------------------------------------------
-strat='median'
+
+def simple_imputer_iml2(strat, features_pd):
+
+    # Writing full dataset to multidimensional np array:
+    md_list = np.array([])
+
+    fulldata = features_pd
+
+    # From the pd_dataframe given by the csv, the 3d Array "md_list" is constructed.
+    #    Dimensions: 0:time
+    #                1:Labels
+    #                2:pid
+    list_pid = fulldata.pid.unique()  # get a list of all pids present in the dataset
+    md_list = fulldata.loc[fulldata['pid'] == list_pid[0]].to_numpy()
+    list_pid = np.delete(list_pid, 0)
+    for pid in list_pid:
+        this_data = fulldata.loc[fulldata['pid'] == pid].to_numpy()
+        md_list = np.dstack((md_list, this_data))
+    [n_times, n_meas, n_pid] = np.shape(md_list)
+
+    # Other preparations
+    # ------------------
+    # Find averages over all patients for each measurement to get a bias value
+    #   (needed to perform meadian or mean imputing in cols full of nan)
+    fulldata_np = fulldata.to_numpy()
+    (fulldata_rows, fulldata_cols) = np.shape(fulldata_np)
+    avg_fulldata = np.nanmean(fulldata_np, axis=0)
+
+    md_list_imp = md_list
+
+    print("Finished prep. Imputing starts...")
+
+    for pid in range(n_pid):
+        this_pid = md_list[:, :, pid]
+
+        if (strat != 'constant'):
+            # Check if there is a col with all nan-values:
+            isnan_bool = np.all(np.isnan(this_pid), axis=0)
+            for isnan_col in range(np.shape(isnan_bool)[0]):
+                if isnan_bool[isnan_col]:
+                    # If all nan: isnan_col is true thus set one entry of col to avg over all patients:
+                    this_pid[2, isnan_col] = avg_fulldata[isnan_col]
+
+        # Imputers:
+        imputer = SimpleImputer(missing_values=np.nan, strategy=strat)
+
+        this_pid_imp = imputer.fit_transform(this_pid)
+
+        # write imputed person data to the multi dimensional list
+
+        md_list_imp[:, :, pid] = this_pid_imp
+
+        # The md_list_imp needs to be written back to a pandas dataframe in the same shape as before.
+        features_imp_pd = md_list2pdSeries(md_list_imp, fulldata.columns.tolist())
+
+        return features_imp_pd
 
 
-print("Starts. Reading in Data...")
-totaltime_start=time.perf_counter()
+def simpleimp_mean(PATH_TEST_FEATURES, PATH_TRAIN_FEATURES):
+    # This functions Imputes the test and train data using mean strategy and return pd_frames
+    test_features_pd = pd.read_csv(PATH_TEST_FEATURES)
+    train_features_pd = pd.read_csv(PATH_TRAIN_FEATURES)
 
-fulldata=pd.read_csv('Data_2/test_features.csv')
+    train_features_simpleimp = simple_imputer_iml2('mean', train_features_pd)
+    test_features_simpleimp = simple_imputer_iml2('mean', test_features_pd)
 
-random.seed(1234)
-
-#Writing full dataset to multidimensional np array:
-md_list=np.array([])
-
-#----------
-print("Preparing prerequisits...")
-print_elapsed_time(totaltime_start)
-#----------
-
-list_pid=fulldata.pid.unique()
-md_list=fulldata.loc[fulldata['pid'] == list_pid[0]].to_numpy()
-list_pid=np.delete(list_pid,0)
-for pid in list_pid:
-    this_data = fulldata.loc[fulldata['pid'] == pid].to_numpy()
-    md_list=np.dstack((md_list,this_data))
-[n_times,n_meas,n_pid]=np.shape(md_list)
-
-#Find averages over all patients for each measurement to get a bias value
-#   (needed to perform meadian or mean imputing in cols full of nan)
-fulldata_np=fulldata.to_numpy()
-#print(np.shape(fulldata_np)) #for debugging
-(fulldata_rows, fulldata_cols)=np.shape(fulldata_np)
-avg_fulldata=np.nanmean(fulldata_np,axis=0)
-
-md_list_impmean = md_list
-md_list_impmedian = md_list
-md_list_impconst = md_list
-
-print("Finished prep. Imputing starts...")
-print_elapsed_time(totaltime_start)
-
-#for pid in [0]:
-for pid in range(n_pid):
-    this_pid = md_list[:,:,pid]
-
-    if (strat!='constant'):
-        #Check if there is a col with all nan-values:
-        isnan_bool=np.all(np.isnan(this_pid),axis=0)
-        for isnan_col in range(np.shape(isnan_bool)[0]):
-            if isnan_bool[isnan_col]:
-                #If all nan: isnan_col is true thus set one entry of col to avg over all patients:
-                this_pid[2,isnan_col]=avg_fulldata[isnan_col]
-
-    #print(np.shape(this_pid)) #for debugging
-
-    #Imputers:
-    imp_mean = SimpleImputer(missing_values=np.nan,strategy=strat)
-
-    this_pid_impmean=imp_mean.fit_transform(this_pid)
-    #print(this_pid_impmean)
-
-    #write imputed Persondata to the muldidimensional list
-    #print(np.shape(md_list_impmean))
-    #print(np.shape(this_pid_impmean))
-    md_list_impmean[:,:,pid]=this_pid_impmean
-
-#print(md_list_impmean[:, 4:9, pid])
-print("Imputing finished, starting to write the multidimensional lists to csv's...")
-print_elapsed_time(totaltime_start)
+    return train_features_simpleimp, test_features_simpleimp
 
 
-md_list2pdSeries(md_list_impmean,fulldata.columns.tolist(),strat)
-print("Finished writing imputed Data to file...")
-print_elapsed_time(totaltime_start)
+def simpleimp_median(PATH_TEST_FEATURES, PATH_TRAIN_FEATURES):
+    # This functions Imputes the test and train data using median strategy and return pd_frames
 
-print("Finished Execution, new Datasets in correspondig Files.")
-print_elapsed_time(totaltime_start)
+    test_features_pd = pd.read_csv(PATH_TEST_FEATURES)
+    train_features_pd = pd.read_csv(PATH_TRAIN_FEATURES)
+
+    train_features_simpleimp = simple_imputer_iml2('median', train_features_pd)
+    test_features_simpleimp = simple_imputer_iml2('median', test_features_pd)
+
+    return train_features_simpleimp, test_features_simpleimp
+
+
+def simpleimp_constant(PATH_TEST_FEATURES, PATH_TRAIN_FEATURES):
+    # This functions Imputes the test and train data using constant (0) strategy and return pd_frames
+    test_features_pd = pd.read_csv(PATH_TEST_FEATURES)
+    train_features_pd = pd.read_csv(PATH_TRAIN_FEATURES)
+
+    train_features_simpleimp = simple_imputer_iml2('constant', train_features_pd)
+    test_features_simpleimp = simple_imputer_iml2('constant', test_features_pd)
+
+    return train_features_simpleimp, test_features_simpleimp
+
+# --------------------------------------------------------------------------------------------------
+# CODE
+# ----
+
+# NO CODE!!! USE AS IMPORTFILE.
