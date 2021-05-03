f0b5fdd6170a3faf86fd9b5f8388def6beff2453
sitobler <sitobler@student.ethz.ch>
2021-05-03 18:15:32 +0200

    General additions Task3.

--- Task 3/Code_3/main.py
+++ Task 3/Code_3/main.py
@@ -9,4 +9,43 @@
 
 import numpy as np
 from sklearn.metrics import f1_score
+import tensorflow as tf
+from tensorflow import keras
 
+
+# -------------------------------------------------------------------------------------------------
+# FUNCTIONS
+# ---------
+
+def print_elapsed_time(starttime):
+    time_now = time.perf_counter()
+    elapsed_time = time_now-starttime
+    print("    Time elapsed since start: %.2f s" % elapsed_time)
+
+
+# -------------------------------------------------------------------------------------------------
+# VARIABLES
+# ---------
+
+PATH_TRAIN_DATA = 'Data_3/train.csv'
+PATH_TEST_DATA = 'Data_3/test.csv'
+PATH_SAMPLE_FILE = 'Data_3/sample.csv'
+# ==================================================================================================
+# DATA STUFF
+# ----------
+print('=====Execution starts.======')
+print('=====   Preparations...')
+
+random.seed(1234)
+
+current_time_str = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+train_pd = pd.read_csv(PATH_TRAIN_DATA)
+test_pd = pd.read_csv(PATH_TEST_DATA)
+
+sample_pd = pd.read_csv(PATH_SAMPLE_FILE)
+
+
+
+# ==================================================================================================
+# VARIABLES
+# ----------
