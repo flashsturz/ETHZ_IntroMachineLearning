f7f0971eba1bc11d8fa80e3ba2dc865ba33f288c
Janik Baumer <janik.baumer@gmail.com>
2021-05-03 10:17:41 +0200

    initial directory structure task3

---
+++ Task 3/Code_3/main.py
@@ -0,0 +1,12 @@
+# Janik Baumer, Flavio Regenass, Simon Tobler
+# jbaumer, flavior, sitobler
+#--------------------------------------------------------------------------------------------------
+# Description Task 3
+# Classification of mutations of a human antibody protein into active (1) and inactive (0) based on provided mutation information
+# active mutation (1): protein retains original functions
+# inactive mutation (0): protein looses its function
+#--------------------------------------------------------------------------------------------------
+
+import numpy as np
+from sklearn.metrics import f1_score
+
